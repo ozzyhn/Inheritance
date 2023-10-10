1e8e61b0b954327ec49720e5cdd6a368180ab021
Oguzhan <abaroguzhan@gmail.com>
2023-10-10 21:13:45 +0300

    Inheritance-ODEV
    Inheritance Examples With Dinosaours

---
+++ Ödev3/src/Main.java
@@ -0,0 +1,41 @@
+import Dinosaours.Raptor;
+import Dinosaours.Tavuk;
+import Dinosaours.Trex;
+
+public class Main {
+	
+	
+	public static void main (String[]args) {
+		
+		//ornek1();
+		//ornek2();
+		//ornek3();
+	}
+
+	private static void ornek3() {
+		new Tavuk().GetName();
+		new Tavuk().Eat();
+		new Tavuk().Speak();
+		new Tavuk().Run();
+		new Tavuk().Fly();
+		
+	}
+
+	private static void ornek2() {
+		//new Trex().GetName();
+		//new Trex().Eat();
+		//new Trex().Speak();
+		//new Trex().Run();
+		//new Trex().sırtınıKaşı();
+		
+	}
+
+	private static void ornek1() {
+		// new Raptor().GetName();
+		//new Raptor().Eat();
+		//new Raptor().Speak();
+		//new Raptor().Run();
+		
+	}
+
+}
